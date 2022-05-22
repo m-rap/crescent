61b13816b045ce31778c04d5c4ba184751f6cfe6
rian <mohammad.rian.ap@gmail.com>
2022-05-22 08:52:51 +0700

    update aes implementation (untested)

--- client/app.js
+++ client/app.js
@@ -1,3 +1,10 @@
+
+/// <reference path="forge.min.js" />
+/// <reference path="https://unpkg.com/node-forge@1.0.0/dist/forge.min.js" />
+/// <reference path="react.production.min.js" />
+/// <reference path="react-dom.production.min.js" />
+/// <reference path="babel.min.js" />
+
 class App extends React.Component {
 
     state = {
@@ -16,6 +23,8 @@ class App extends React.Component {
         const rsa = forge.pki.rsa;
         const pki = forge.pki;
 
+        console.log("nego state: " + this.state.negoState);
+
         if (this.state.negoState == 0) {
             const keypair = rsa.generateKeyPair({bits: 2048});
             let pubKeyPem = pki.publicKeyToPem(keypair.publicKey);
@@ -31,16 +40,29 @@ class App extends React.Component {
             const res = await fetch("http://localhost:8080/api", opts);
             const resJson = await res.json();
 
+            console.log("got response " + JSON.stringify(resJson));
+
             let encryptedSymKey = forge.util.decode64(resJson.d4);
             this.state.symKey = keypair.privateKey.decrypt(encryptedSymKey, 'RSA-OAEP', {md: forge.md.sha256.create()});
+
+            console.log("decrypted symkey: " + encryptedSymKey);
+
             this.state.negoState = 1;
         } else if (this.state.negoState == 1) {
             let data = "data rahasia lho";
 
-            let cipher = forge.rc2.createEncryptionCipher(this.state.symKey);
-            cipher.start();
+            console.log("encrypting data before send to server");
+
+            console.log("create encryption cipher");
+            let cipher = forge.cipher.createCipher('AES-CBC', this.state.symKey);
+            let iv = forge.random.getBytesSync(16);
+
+            console.log("start cipher");
+            cipher.start({iv: iv});
             cipher.update(forge.util.createBuffer(data));
             cipher.finish();
+            console.log("cipher finished");
+
             let encryptedData = cipher.output.getBytes();
             let encryptedData64 = forge.util.encode64(encryptedData);
 
@@ -53,9 +75,11 @@ class App extends React.Component {
             const res = await fetch("http://localhost:8080/Api", opts);
             const resJson = await res.json();
 
+            console.log("got response " + JSON.stringify(resJson));
+
             let resEncryptedData = forge.util.decode64(resJson.d2);
-            let decCipher = forge.rc2.createDecryptionCipher(this.state.symKey);
-            decCipher.start();
+            let decCipher = forge.cipher.createDecipher(this.state.symKey);
+            decCipher.start({iv: iv});
             decCipher.update(resEncryptedData);
             decCipher.finish();
             let resData = JSON.parse(decCipher.output.getBytes().toString());
--- server/main.go
+++ server/main.go
@@ -12,6 +12,7 @@ import (
 	"encoding/pem"
 	"fmt"
 	"math/rand"
+	"strconv"
 	"strings"
 	"time"
 
@@ -22,6 +23,9 @@ import (
 type EncNegoClientParams struct {
 	D1 string `json:"d1"`
 	D2 string `json:"d2"`
+	D3 string `json:"d3"`
+	D4 string `json:"d4"`
+	D5 string `json:"d5"`
 }
 
 type ClientInfo struct {
@@ -38,27 +42,35 @@ func Home(c *gin.Context) {
 
 func Negotiate(c *gin.Context) {
 	state := 0
+
+	//raw, _ := c.GetRawData()
+	//fmt.Printf("request %v\n", string(raw))
+
 	var params EncNegoClientParams
+	c.ShouldBindJSON(&params)
 
 	id, err := c.Cookie("uuid")
 
 	if err != nil {
 		fmt.Printf("id not exists: %s:%T,%s:%T\n", id, id, err, err)
 	} else {
-		state = 1
+		//state = 1
+	}
+
+	fmt.Printf("params d5 %s\n", params.D5)
+	if params.D5 != "" {
+		state, _ = strconv.Atoi(params.D5)
+		fmt.Printf("request nego state %s %d\n", params.D5, state)
 	}
 
 	switch state {
 	case 0:
-		c.BindJSON(&params)
 		if strings.Trim(params.D1, " ") == "" {
 			c.JSON(200, gin.H{
 				"msg": "invalid params",
 			})
 			return
 		}
-
-		fmt.Printf("d1 %s\n", params.D1)
 
 		//pubKeyDER, _ := base64.StdEncoding.DecodeString(params.D1)
 		//pubKeyInterface, err := x509.ParsePKIXPublicKey(pubKeyDER)
@@ -72,15 +84,16 @@ func Negotiate(c *gin.Context) {
 		//	fmt.Printf("can't cast pubkey\n")
 		//}
 
-		pubKeyPem, _ := base64.StdEncoding.DecodeString(params.D1)
-		pubKeyPemBlock, _ := pem.Decode(pubKeyPem)
+		//pubKeyPem, _ := base64.StdEncoding.DecodeString(params.D1)
+		pubKeyPemBlock, _ := pem.Decode([]byte(params.D1))
 		if pubKeyPemBlock == nil {
 			fmt.Printf("can't find pem block.\n")
 			c.Abort()
 			return
 		}
 		fmt.Printf("pem block: %v\n", pubKeyPemBlock)
-		pubKey, err := x509.ParsePKCS1PublicKey(pubKeyPemBlock.Bytes)
+		pubKeyInterface, err := x509.ParsePKIXPublicKey(pubKeyPemBlock.Bytes)
+		pubKey, _ := pubKeyInterface.(*rsa.PublicKey)
 		if err != nil {
 			fmt.Printf("failed parse pem block: %s\n", err.Error())
 			c.Abort()
@@ -94,22 +107,37 @@ func Negotiate(c *gin.Context) {
 		encryptedSymKey, _ := rsa.EncryptOAEP(sha256.New(), cRand.Reader, pubKey, symKey, nil)
 		encryptedSymKeyB64 := base64.StdEncoding.EncodeToString(encryptedSymKey)
 
+		fmt.Println("set cookie")
 		c.SetCookie("uuid", id, 10*365*24*60*60, "/", "localhost", false, true)
+		fmt.Println("write response in json")
 		c.JSON(200, gin.H{
 			"d4": encryptedSymKeyB64,
 		})
+		fmt.Println("abort because still state 0")
+		c.Abort()
 
 	case 1:
 		clientInfo := clientInfoMap[id]
 		c.BindJSON(&params)
 		aesCipher, _ := aes.NewCipher(clientInfo.SymKey)
-		encryptedData, _ := base64.StdEncoding.DecodeString(params.D2)
+		d2Arr := strings.Split(params.D2, "\n")
+		if len(d2Arr) < 2 {
+			c.JSON(200, gin.H {
+				"msg": "invalid data"
+			})
+			c.Abort()
+			return
+		}
+		iv, _ := base64.StdEncoding.DecodeString(d2Arr[0])
+		decrypter := cipher.NewCBCDecrypter(aesCipher, iv)
+		encryptedData, _ := base64.StdEncoding.DecodeString(d2Arr[1])
 		data := make([]byte, len(encryptedData))
-		aesCipher.Decrypt(data, encryptedData)
+		decrypter.Decrypt(data, encryptedData)
 		var dataJson map[string]interface{}
 		json.Unmarshal(data, &dataJson)
 		c.Set("data", dataJson)
 		c.Set("cipher", aesCipher)
+		c.set("iv", iv)
 		c.Next()
 	}
 }
@@ -119,12 +147,15 @@ func Api(c *gin.Context) {
 	tmp, _ = c.Get("data")
 	data := tmp.(map[string]interface{})
 	tmp, _ = c.Get("cipher")
-	cipher := tmp.(cipher.Block)
+	aesCipher := tmp.(cipher.Block)
+	tmp, _ = c.Get("iv")
+	iv := tmp.(string)
 	fmt.Println(data)
 
 	responseData := []byte("{data: data rahasia balasan lho}")
 	resDataEncrypted := make([]byte, len(responseData))
-	cipher.Encrypt(resDataEncrypted, responseData)
+	encrypter := cipher.NewCBCEncrypter(aesCipher, iv)
+	encrypter.Encrypt(resDataEncrypted, responseData)
 	resDataEncrypted64 := base64.StdEncoding.EncodeToString(resDataEncrypted)
 
 	c.JSON(200, gin.H{
